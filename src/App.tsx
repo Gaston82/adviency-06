2334a9a13e19b698406241252d41380b72cd4b3a
Gaston82 <cabezaartaza@hotmail.com>
2023-12-06 18:52:37 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+const App = (): React.ReactElement => {
+  return (
+    <section className="container">
+      <h1>Regalos</h1>
+    </section>
+  );
+};
+export default App;
