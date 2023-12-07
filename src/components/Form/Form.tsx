df7f32b3fae77152e5678dbdf2521a85112d8eaf
Gaston82 <cabezaartaza@hotmail.com>
2023-12-07 01:11:05 +0100

    Add form functionality

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Form.css";
 
-const Form = (): React.ReactElement => {
+interface FormProps {
+  onAddGifts: (inputValue: string) => void;
+}
+
+const Form = ({ onAddGifts }: FormProps): React.ReactElement => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onAddGifts(inputValue);
+    setInputValue("");
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
         className="form__input"
         type="text"
         placeholder="socks, jeans,t-shirt"
+        value={inputValue}
+        onChange={(event) => {
+          setInputValue(event.target.value);
+        }}
       />
       <button className="form__button" type="submit">
         Add
