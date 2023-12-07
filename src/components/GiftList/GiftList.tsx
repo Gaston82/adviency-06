ab74d423d5bc1d22ca656c09d2dd704f9553269d
Gaston82 <cabezaartaza@hotmail.com>
2023-12-07 18:47:26 +0100

    Implement onDeleteGift function on Gift component

--- src/components/GiftList/GiftList.tsx
+++ src/components/GiftList/GiftList.tsx
@@ -4,13 +4,17 @@ import "./GiftList.css";
 
 interface GiftListProps {
   gifts: GiftStructure[];
+  onDeleteGift: (giftId: string) => void;
 }
 
-const GiftList = ({ gifts }: GiftListProps): React.ReactElement => {
+const GiftList = ({
+  gifts,
+  onDeleteGift,
+}: GiftListProps): React.ReactElement => {
   return (
     <ul className="gift-list">
       {gifts.map((gift) => (
-        <Gift gift={gift} key={gift.id} />
+        <Gift gift={gift} key={gift.id} onDeleteGift={onDeleteGift} />
       ))}
     </ul>
   );
