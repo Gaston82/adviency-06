b4b8e17f854d85f80e5a957e391ad29973cdd206
Gaston82 <cabezaartaza@hotmail.com>
2023-12-08 00:54:10 +0100

    Install react testing library, add vite config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,24 @@
 module.exports = {
-  root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  ignorePatterns: ["/*.js", "/*.cjs"],
+  settings: {
+    react: {
+      version: "detect",
+    },
   },
-}
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  plugins: ["react-refresh", "jsx-a11y", "react-hooks"],
+  rules: {
+    "react-refresh/only-export-components": "warn",
+    "no-console": "error",
+  },
+};
