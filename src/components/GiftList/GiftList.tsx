e31a56a483c53a865280f62a2ff8cd96fa1982bd
Gaston82 <cabezaartaza@hotmail.com>
2023-12-07 19:04:01 +0100

    Implement deleteGifts function on GiftList component

--- src/components/GiftList/GiftList.tsx
+++ src/components/GiftList/GiftList.tsx
@@ -5,18 +5,25 @@ import "./GiftList.css";
 interface GiftListProps {
   gifts: GiftStructure[];
   onDeleteGift: (giftId: string) => void;
+  onDeleteGifts: () => void;
 }
 
 const GiftList = ({
   gifts,
   onDeleteGift,
+  onDeleteGifts,
 }: GiftListProps): React.ReactElement => {
   return (
-    <ul className="gift-list">
-      {gifts.map((gift) => (
-        <Gift gift={gift} key={gift.id} onDeleteGift={onDeleteGift} />
-      ))}
-    </ul>
+    <>
+      <ul className="gift-list">
+        {gifts.map((gift) => (
+          <Gift gift={gift} key={gift.id} onDeleteGift={onDeleteGift} />
+        ))}
+      </ul>
+      <button className="form__button" onClick={onDeleteGifts}>
+        Delete All
+      </button>
+    </>
   );
 };
 
