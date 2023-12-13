14d93f3c8be79556b6ac8e2333edb5ded7ed53c8
Gaston82 <cabezaartaza@hotmail.com>
2023-12-13 22:17:00 +0100

    Add dinamic className

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,14 @@
+import "./Button.css";
 interface ButtonProps {
   text: string;
   action?: () => void;
   type?: "button" | "reset" | "submit";
-  className: string;
+  className?: string;
 }
 
 const Button = ({ text, action, type, className }: ButtonProps) => {
   return (
-    <button className={className} type={type} onClick={action}>
+    <button className={`button ${className}`} type={type} onClick={action}>
       {text}
     </button>
   );
