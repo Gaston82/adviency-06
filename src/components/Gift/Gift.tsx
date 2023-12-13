548f34ec752304bee59949baa747bed527436508
Gaston82 <cabezaartaza@hotmail.com>
2023-12-13 20:24:28 +0100

    Add recipient style and display recipient name

--- src/components/Gift/Gift.tsx
+++ src/components/Gift/Gift.tsx
@@ -10,7 +10,10 @@ const Gift = ({ gift, onDeleteGift }: GiftProps): React.ReactElement => {
   return (
     <li className="gift">
       <img src={gift.image} alt="Gift photography" width={40} height={40} />
-      <h3>{gift.name}</h3>
+      <div>
+        <h3>{gift.name}</h3>
+        <span className="gift__recipient">{gift.to}</span>
+      </div>
       <span>{gift.quantity}</span>
       <button className="form__button" onClick={() => onDeleteGift(gift.id)}>
         Delete
